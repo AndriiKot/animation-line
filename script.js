b09cdbc9ca6e7c5232f129cc646c3fe6e2cfb65b
AndriiKot <a_kocyuba@ukr.net>
2023-03-18 01:39:08 +0100

    ADD function setIntervar and rotate

--- script.js
+++ script.js
@@ -26,6 +26,31 @@ let final_str
 final_str = `linear-gradient(${deg}deg,${str}`
 final_str = final_str.slice(0,final_str.length-1)+')'
 
+// element.style.transform = 'rotate(90deg)'
 element.style.background = final_str
-console.log(final_str)
 
+
+    let nIntervId;
+    
+
+    function ch() {
+      nIntervId = setInterval(m,40);
+    }
+      let int_1 = 0
+      let int_2 = 0
+    function m(){
+      
+      int_2 = int_1 + 1
+      element.style.transform = element.style.transform == `rotate(${int_1}deg)` ? `rotate(${int_2}deg)` : `rotate(${int_1}deg)`;
+      if(int_2 > 360 || int_1 > 360){
+        int_1 = 0
+      }
+      int_1 += 1
+      console.log(int_1)
+      console.log(int_2)
+  }
+
+
+  
+
+ 
